54ebd5a5ca6b3e3d2afcc9a9d04adcdd98bdeb0b
gbl08ma <gbl08ma@gmail.com>
2019-09-25 18:06:48 +0100

    Implement proxies list command

---
+++ cmd/proxies-list.go
@@ -0,0 +1,120 @@
+// Package cmd implements fyde-cli commands
+package cmd
+
+/*
+Copyright © 2019 Fyde, Inc. <hello@fyde.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"fmt"
+
+	"github.com/jedib0t/go-pretty/table"
+	"github.com/jedib0t/go-pretty/text"
+	"github.com/spf13/cobra"
+
+	apiproxies "github.com/oNaiPs/fyde-cli/client/access_proxies"
+)
+
+// proxiesListCmd represents the list command
+var proxiesListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List proxies",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		err := preRunCheckAuth(cmd, args)
+		if err != nil {
+			return err
+		}
+
+		err = preRunFlagChecks(cmd, args)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		params := apiproxies.NewListProxiesParams()
+		setSort(cmd, params)
+		completePayload := []*apiproxies.ListProxiesOKBodyItems0{}
+		total := 0
+		cutStart, cutEnd, err := forAllPages(cmd, params, func() (int, int64, error) {
+			resp, err := global.Client.AccessProxies.ListProxies(params, global.AuthWriter)
+			if err != nil {
+				return 0, 0, err
+			}
+			completePayload = append(completePayload, resp.Payload...)
+			total = int(resp.Total)
+			return len(resp.Payload), resp.Total, err
+		})
+		if err != nil {
+			return processErrorResponse(err)
+		}
+		completePayload = completePayload[cutStart:cutEnd]
+
+		tw := table.NewWriter()
+		tw.Style().Format.Header = text.FormatDefault
+		tw.AppendHeader(table.Row{
+			"ID",
+			"Name",
+			"Location",
+			"Proxy host:port",
+			"Resources",
+			"Granted req.",
+			"Total req.",
+			"Last access",
+		})
+		tw.SetAllowedColumnLengths([]int{36, 30, 30, 30, 9, 12, 12, 30})
+
+		for _, item := range completePayload {
+			lastAccess := fmt.Sprint(item.LastAccessAt)
+			if item.LastAccessAt == nil {
+				lastAccess = "never"
+			}
+
+			tw.AppendRow(table.Row{
+				item.ID,
+				item.Name,
+				item.Location,
+				fmt.Sprintf("%s:%d", item.Host, item.Port),
+				item.AccessResourcesCount,
+				item.AccessCount.Granted,
+				item.AccessCount.Granted + item.AccessCount.Denied,
+				lastAccess,
+			})
+		}
+
+		result, err := renderListOutput(cmd, completePayload, tw, total)
+		cmd.Println(result)
+		return err
+	},
+}
+
+func init() {
+	proxiesCmd.AddCommand(proxiesListCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// proxiesListCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// proxiesListCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	initPaginationFlags(proxiesListCmd)
+	initSortFlags(proxiesListCmd)
+	initOutputFlags(proxiesListCmd)
+}
