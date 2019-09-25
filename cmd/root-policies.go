20d932b4ab75250300c97e9523dc516ea0559d6d
gbl08ma <gbl08ma@gmail.com>
2019-09-25 18:34:52 +0100

    Implement list policies command

---
+++ cmd/root-policies.go
@@ -0,0 +1,45 @@
+// Package cmd implements fyde-cli commands
+package cmd
+
+/*
+Copyright © 2019 Fyde, Inc. <hello@fyde.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// policiesCmd represents the policies command
+var policiesCmd = &cobra.Command{
+	Use:   "policies",
+	Short: "Operations on policies",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Println("policies called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(policiesCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// policiesCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// policiesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
