e3738f1dde521da29316bd1349ee2b2be2af2a09
gbl08ma <gbl08ma@gmail.com>
2019-12-07 13:49:02 +0000

    Add support for deleting devices

---
+++ cmd/delete-device.go
@@ -0,0 +1,118 @@
+// Package cmd implements fyde-cli commands
+package cmd
+
+/*
+Copyright © 2019 Fyde, Inc. <hello@fyde.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+
+	apidevices "github.com/fyde/fyde-cli/client/devices"
+)
+
+// deviceDeleteCmd represents the delete command
+var deviceDeleteCmd = &cobra.Command{
+	Use:     "delete",
+	Aliases: []string{"remove", "rm"},
+	Short:   "Delete devices",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		err := preRunCheckAuth(cmd, args)
+		if err != nil {
+			return err
+		}
+
+		err = preRunFlagChecks(cmd, args)
+		if err != nil {
+			return err
+		}
+
+		if len(args) == 0 {
+			return fmt.Errorf("missing device ID argument")
+		}
+
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		deviceIDs := make([]int64, len(args))
+		var err error
+		for i, arg := range args {
+			deviceIDs[i], err = strconv.ParseInt(arg, 10, 64)
+			if err != nil {
+				return err
+			}
+		}
+
+		delete := func(id int64) error {
+			gparams := apidevices.NewGetDeviceParams()
+			gparams.SetID(id)
+			resp, err := global.Client.Devices.GetDevice(gparams, global.AuthWriter)
+			if err != nil {
+				return processErrorResponse(err)
+			}
+
+			params := apidevices.NewDeleteDeviceParams()
+			params.SetUserID(resp.Payload.User.ID)
+			params.SetDeviceID(resp.Payload.ID)
+
+			_, err = global.Client.Devices.DeleteDevice(params, global.AuthWriter)
+			if err != nil {
+				return processErrorResponse(err)
+			}
+			return nil
+		}
+
+		i := 0
+		for _, deviceID := range deviceIDs {
+			err = delete(deviceID)
+			if err != nil {
+				if !loopControlContinueOnError(cmd) {
+					return err
+				}
+				cmd.PrintErrln(err)
+			} else {
+				// only keep successful deletions in list of deviceIDs
+				// this rewrites the array in place and lets us "delete" as we iterate
+				// (junk is removed after the loop)
+				deviceIDs[i] = deviceID
+				i++
+			}
+		}
+		// remove junk left at end of slice
+		deviceIDs = deviceIDs[:i]
+
+		printMultiOpOutput(cmd, "Device", deviceIDs, "deleted")
+		return nil
+	},
+}
+
+func init() {
+	devicesCmd.AddCommand(deviceDeleteCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// deviceDeleteCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// deviceDeleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	initLoopControlFlags(deviceDeleteCmd)
+}
