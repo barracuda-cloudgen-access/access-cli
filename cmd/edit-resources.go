eb143860d88ff6dea77bdd0efe50ba085c3f07e6
Jose Pereira <jpereira@barracuda.com>
2021-05-18 18:31:16 +0100

    Add fixed last octet to resources

--- cmd/edit-resources.go
+++ cmd/edit-resources.go
@@ -18,11 +18,15 @@ limitations under the License.
 */
 
 import (
+	"log"
+	"strconv"
+
 	"github.com/go-openapi/strfmt"
 	"github.com/spf13/cobra"
 
 	apiresources "github.com/barracuda-cloudgen-access/access-cli/client/access_resources"
 	"github.com/barracuda-cloudgen-access/access-cli/models"
+	"github.com/barracuda-cloudgen-access/access-cli/serial"
 )
 
 // resourcesEditCmd represents the edit command
@@ -76,7 +80,18 @@ var resourcesEditCmd = &cobra.Command{
 						}
 					},
 					func(s []string) { resource.WildcardExceptions = s },
-					func(s string) { resource.Notes = &s })
+					func(s string) { resource.Notes = &s },
+					func(s string) {
+						resource.FixedLastOctet = &serial.NullableOptionalInt{}
+						if s == "null" {
+							return
+						}
+						i, err := strconv.ParseInt(s, 10, 9)
+						if err != nil {
+							log.Fatal(err)
+						}
+						resource.FixedLastOctet.Value = &i
+					})
 				if err != nil {
 					return nil, err
 				}
@@ -195,6 +210,14 @@ func init() {
 			FlagName:        "notes",
 			FlagDescription: "specify notes for the resource",
 			VarType:         "string",
+			Mandatory:       false,
+			DefaultValue:    "",
+		},
+		inputField{
+			Name:            "Fixed Last Octet",
+			FlagName:        "fixed_last_octet",
+			FlagDescription: "forces the agent to bind the resource to a local IP in the format 192.0.2.X (null to disable)",
+			VarType:         "string", // use string to read "null" pseudo value
 			Mandatory:       false,
 			DefaultValue:    "",
 		})
---
+++ serial/nullable-optional-int-type.go
@@ -0,0 +1,39 @@
+// Package serial implements access-cli serializables
+package serial
+
+import "encoding/json"
+
+/*
+Copyright © 2020 Barracuda Networks, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+type NullableOptionalInt struct {
+	Value *int64
+}
+
+// MarshalJSON returns the NullableOptionalInt as JSON
+func (n NullableOptionalInt) MarshalJSON() ([]byte, error) {
+	return json.Marshal(n.Value)
+}
+
+// UnmarshalJSON sets the NullableOptionalInt from JSON
+func (n *NullableOptionalInt) UnmarshalJSON(data []byte) error {
+	var value int64
+	if err := json.Unmarshal(data, &value); err != nil {
+		return err
+	}
+	*n = NullableOptionalInt{Value: &value}
+	return nil
+}
