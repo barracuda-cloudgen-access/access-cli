4445d76340187ffc1dd14cac59b9a7f174afbc4f
Daniela Buhr <dbuhr@barracuda.com>
2023-12-06 14:03:40 -0800

    fix(): enrollment api definitions and  users enrollment commands: generate, change, get, revoke to include classifications

--- cmd/user-enrollment.go
+++ cmd/user-enrollment.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/go-openapi/strfmt"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/spf13/cobra"
@@ -67,6 +68,7 @@ var enrollmentGenerateCmd = &cobra.Command{
 		tw.Style().Format.Header = text.FormatDefault
 		tw.AppendHeader(table.Row{
 			"ID",
+			"Classification",
 			"Slots",
 			"Expiration",
 			"URL",
@@ -74,8 +76,9 @@ var enrollmentGenerateCmd = &cobra.Command{
 		tw.SetColumnConfigs([]table.ColumnConfig{
 			{Number: 1, WidthMax: 15},
 			{Number: 2, WidthMax: 10},
-			{Number: 3, WidthMax: 30},
-			{Number: 4, WidthMax: 140},
+			{Number: 3, WidthMax: 10},
+			{Number: 4, WidthMax: 30},
+			{Number: 5, WidthMax: 140},
 		})
 		createdList := []*apiusers.GenerateEnrollmentLinkCreatedBody{}
 
@@ -83,6 +86,23 @@ var enrollmentGenerateCmd = &cobra.Command{
 			params := apiusers.NewGenerateEnrollmentLinkParams()
 			setTenant(cmd, params)
 			params.SetID(arg)
+
+			device_classification := cmd.Flag("classification").Value.String()
+			ref_count, err := cmd.Flags().GetInt("slots")
+			if err != nil {
+				return err
+			}
+			enrollment := apiusers.GenerateEnrollmentLinkBody{
+				Enrollment: &apiusers.GenerateEnrollmentLinkParamsBodyEnrollment{
+					DeviceClassification: &device_classification,
+					Refcount:             int64(ref_count),
+				},
+			}
+
+			params.SetEnrollment(enrollment)
+			if err != nil {
+				return err
+			}
 
 			resp, err := global.Client.Users.GenerateEnrollmentLink(params, global.AuthWriter)
 			if err != nil {
@@ -94,6 +114,7 @@ var enrollmentGenerateCmd = &cobra.Command{
 
 				tw.AppendRow(table.Row{
 					fmt.Sprintf("[ERR] %v", arg),
+					"-",
 					"-",
 					"-",
 					processErrorResponse(err),
@@ -109,6 +130,7 @@ var enrollmentGenerateCmd = &cobra.Command{
 
 			tw.AppendRow(table.Row{
 				arg,
+				resp.GetPayload().DeviceClassification,
 				resp.GetPayload().Count,
 				resp.GetPayload().Expiration,
 				resp.GetPayload().URL,
@@ -131,11 +153,34 @@ var enrollmentRevokeCmd = &cobra.Command{
 		}
 
 		tw, j := multiOpBuildTableWriter()
+		device_classification := cmd.Flag("classification").Value.String()
+		enrollment_id := strfmt.UUID(cmd.Flag("enrollment_id").Value.String())
 
 		for _, arg := range intArgs {
+			if enrollment_id == strfmt.UUID("") && device_classification != "" {
+				enrollment_id, err = _enrollmentIdForClassification(cmd, arg, device_classification)
+				if err != nil {
+					multiOpTableWriterAppend(tw, &j, arg, processErrorResponse(err))
+					if loopControlContinueOnError(cmd) {
+						err = nil
+						continue
+					}
+					return printListOutputAndError(cmd, j, tw, len(intArgs), err)
+				}
+			}
+			if enrollment_id == strfmt.UUID("") {
+				multiOpTableWriterAppend(tw, &j, arg, "no enrollment link found for the specified device classification")
+				if loopControlContinueOnError(cmd) {
+					err = nil
+					continue
+				}
+				return printListOutputAndError(cmd, j, tw, len(intArgs), err)
+			}
+
 			params := apiusers.NewRevokeEnrollmentLinkParams()
 			setTenant(cmd, params)
 			params.SetID(arg)
+			params.EnrollmentID = enrollment_id
 
 			_, err = global.Client.Users.RevokeEnrollmentLink(params, global.AuthWriter)
 			if err != nil {
@@ -156,6 +201,38 @@ var enrollmentRevokeCmd = &cobra.Command{
 		}
 		return printListOutputAndError(cmd, j, tw, len(intArgs), err)
 	},
+}
+
+func _enrollmentIdForClassification(cmd *cobra.Command, user_id int64, classification string) (strfmt.UUID, error) {
+
+	enrollment_id := strfmt.UUID("")
+	params := apiusers.NewGetUserParams()
+	setTenant(cmd, params)
+	params.SetID(user_id)
+
+	resp, err := global.Client.Users.GetUser(params, global.AuthWriter)
+	if err != nil {
+		processErrorResponse(err)
+		return enrollment_id, err
+	}
+
+	if len(resp.Payload.Enrollments) == 0 ||
+		resp.Payload.EnrollmentStatus == "revoked" ||
+		resp.Payload.EnrollmentStatus == "expired" {
+		cmd.Println("No shareable enrollment link available for this user")
+	} else {
+		for _, enrollment := range resp.Payload.Enrollments {
+			if enrollment.DeviceClassification == classification {
+				enrollment_id = enrollment.ID
+				break
+			}
+		}
+	}
+
+	if enrollment_id == strfmt.UUID("") {
+		err = fmt.Errorf("no enrollment link found for the specified device classification")
+	}
+	return enrollment_id, err
 }
 
 // enrollmentChangeCmd represents the change command
@@ -185,26 +262,51 @@ var enrollmentChangeCmd = &cobra.Command{
 			return err
 		}
 
+		classification, err := cmd.Flags().GetString("classification")
+		if err != nil {
+			return err
+		}
+
 		tw := table.NewWriter()
 		tw.Style().Format.Header = text.FormatDefault
 		tw.AppendHeader(table.Row{
 			"ID",
 			"Slots",
+			"Classification",
 			"Expiration",
 			"URL",
 		})
 		tw.SetColumnConfigs([]table.ColumnConfig{
 			{Number: 1, WidthMax: 15},
-			{Number: 2, WidthMax: 10},
-			{Number: 3, WidthMax: 30},
-			{Number: 4, WidthMax: 140},
+			{Number: 2, WidthMax: 5},
+			{Number: 3, WidthMax: 14},
+			{Number: 4, WidthMax: 30},
+			{Number: 5, WidthMax: 140},
 		})
 		editedList := []*apiusers.ChangeEnrollmentLinkSlotsOKBody{}
 
 		for _, arg := range intArgs {
-			params := apiusers.NewChangeEnrollmentLinkSlotsParams()
-			setTenant(cmd, params)
-			params.SetID(arg)
+			enrollment_id, err := _enrollmentIdForClassification(cmd, arg, classification)
+			if err != nil {
+				tw.AppendRow(table.Row{
+					fmt.Sprintf("[ERR] %v", arg),
+					"-",
+					"-",
+					"-",
+					processErrorResponse(err),
+				})
+				editedList = append(editedList, nil)
+				if loopControlContinueOnError(cmd) {
+					cmd.PrintErrln(processErrorResponse(err))
+					continue
+				}
+				return processErrorResponse(err)
+			}
+
+			change_params := apiusers.NewChangeEnrollmentLinkSlotsParams()
+			setTenant(cmd, change_params)
+			change_params.SetID(arg)
+			change_params.SetEnrollmentID(enrollment_id)
 
 			enrollment := &apiusers.ChangeEnrollmentLinkSlotsParamsBodyEnrollment{
 				Refcount: int64(slots),
@@ -213,9 +315,9 @@ var enrollmentChangeCmd = &cobra.Command{
 				UserID:     arg,
 				Enrollment: enrollment,
 			}
-			params.SetRequest(body)
+			change_params.SetRequest(body)
 
-			resp, err := global.Client.Users.ChangeEnrollmentLinkSlots(params, global.AuthWriter)
+			change_resp, err := global.Client.Users.ChangeEnrollmentLinkSlots(change_params, global.AuthWriter)
 			if err != nil {
 				// best possible workaround for https://github.com/go-swagger/go-swagger/issues/1929
 				// (without resorting to fixing the go-swagger code generator)
@@ -240,11 +342,12 @@ var enrollmentChangeCmd = &cobra.Command{
 
 			tw.AppendRow(table.Row{
 				arg,
-				resp.GetPayload().Count,
-				resp.GetPayload().Expiration,
-				resp.GetPayload().URL,
+				change_resp.GetPayload().Count,
+				change_resp.GetPayload().DeviceClassification,
+				change_resp.GetPayload().Expiration,
+				change_resp.GetPayload().URL,
 			})
-			editedList = append(editedList, resp.Payload)
+			editedList = append(editedList, change_resp.Payload)
 		}
 		return printListOutputAndError(cmd, editedList, tw, len(intArgs), err)
 	},
@@ -263,10 +366,13 @@ var enrollmentGetCmd = &cobra.Command{
 
 		cmd.SilenceUsage = true // errors beyond this point are no longer due to malformed input
 
+		jsonArray := make([]map[string]interface{}, 0)
+
 		for _, arg := range intArgs {
 			params := apiusers.NewGetUserParams()
 			setTenant(cmd, params)
 			params.SetID(arg)
+			jsonObject := make(map[string]interface{})
 
 			resp, err := global.Client.Users.GetUser(params, global.AuthWriter)
 			if err != nil {
@@ -277,14 +383,26 @@ var enrollmentGetCmd = &cobra.Command{
 				return processErrorResponse(err)
 			}
 
-			if resp.Payload.Enrollment == nil ||
+			if len(resp.Payload.Enrollments) == 0 ||
 				resp.Payload.EnrollmentStatus == "revoked" ||
 				resp.Payload.EnrollmentStatus == "expired" {
 				cmd.Println("No shareable enrollment link available for this user")
 			} else {
-				cmd.Println(resp.Payload.Enrollment.URL)
+				jsonObject["id"] = resp.Payload.ID
+				for _, enrollment := range resp.Payload.Enrollments {
+					key := enrollment.DeviceClassification
+					value := enrollment.URL
+					jsonObject[key] = value
+				}
 			}
+			jsonArray = append(jsonArray, jsonObject)
 		}
+
+		jsonData, err := renderPrettyJSON(jsonArray)
+		if err != nil {
+			return processErrorResponse(err)
+		}
+		cmd.Println(string(jsonData))
 		return nil
 	},
 }
@@ -302,11 +420,39 @@ var enrollmentEmailCmd = &cobra.Command{
 		}
 
 		tw, j := multiOpBuildTableWriter()
+		device_classification := cmd.Flag("classification").Value.String()
+		slots, err := cmd.Flags().GetInt("slots")
+		if err != nil {
+			return err
+		}
+
+		device_classifications := &apiusers.SendEnrollmentEmailParamsBodyDeviceClassifications{}
+
+		switch device_classification {
+		case "supervised":
+			device_classifications = &apiusers.SendEnrollmentEmailParamsBodyDeviceClassifications{
+				Supervised: int64(slots),
+			}
+		case "managed":
+			device_classifications = &apiusers.SendEnrollmentEmailParamsBodyDeviceClassifications{
+				Managed: int64(slots),
+			}
+		case "personal":
+			device_classifications = &apiusers.SendEnrollmentEmailParamsBodyDeviceClassifications{
+				Personal: int64(slots),
+			}
+		default:
+			return fmt.Errorf("invalid device classification")
+		}
 
 		for _, arg := range intArgs {
 			params := apiusers.NewSendEnrollmentEmailParams()
+
 			setTenant(cmd, params)
 			params.SetID(arg)
+			params.SetDeviceClassifications(apiusers.SendEnrollmentEmailBody{
+				DeviceClassifications: device_classifications,
+			})
 
 			_, err = global.Client.Users.SendEnrollmentEmail(params, global.AuthWriter)
 			if err != nil {
@@ -337,37 +483,56 @@ func init() {
 	enrollmentCmd.AddCommand(enrollmentGetCmd)
 	enrollmentCmd.AddCommand(enrollmentEmailCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// enrollmentCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// enrollmentCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 	initMultiOpArgFlags(enrollmentGenerateCmd, "user", "generate enrollments for", "id", "[]int64")
+	initInputFlags(
+		enrollmentGenerateCmd, "enrollment",
+		inputField{
+			Name:            "Device Classification",
+			FlagName:        "classification",
+			FlagDescription: "specify the device classification for the enrollment link",
+			VarType:         "string",
+			Mandatory:       true,
+			DefaultValue:    "supervised",
+		},
+		inputField{
+			Name:            "Slots",
+			FlagName:        "slots",
+			FlagDescription: "specify the number of slots for the enrollment link",
+			VarType:         "int",
+			Mandatory:       true,
+			DefaultValue:    1,
+			MainField:       true,
+			SchemaName:      "slots",
+		},
+	)
 	initOutputFlags(enrollmentGenerateCmd)
 	initLoopControlFlags(enrollmentGenerateCmd)
 	initTenantFlags(enrollmentGenerateCmd)
 
 	initMultiOpArgFlags(enrollmentRevokeCmd, "user", "revoke enrollments for", "id", "[]int64")
+	enrollmentRevokeCmd.Flags().String("classification", "supervised", "specify the classification")
+	enrollmentRevokeCmd.Flags().String("enrollment_id", "", "the enrollment ID to revoke")
 	initOutputFlags(enrollmentRevokeCmd)
 	initLoopControlFlags(enrollmentRevokeCmd)
 	initTenantFlags(enrollmentRevokeCmd)
 
 	initMultiOpArgFlags(enrollmentChangeCmd, "user", "change enrollments for", "id", "[]int64")
 	enrollmentChangeCmd.Flags().Int("slots", 0, "specify the new number of slots for the enrollment link")
+	enrollmentChangeCmd.Flags().String("classification", "supervised", "specify the classification to change")
+
 	initOutputFlags(enrollmentChangeCmd)
 	initLoopControlFlags(enrollmentChangeCmd)
 	initTenantFlags(enrollmentChangeCmd)
 
 	initMultiOpArgFlags(enrollmentGetCmd, "user", "get enrollments for", "id", "[]int64")
+
 	initLoopControlFlags(enrollmentGetCmd)
 	initTenantFlags(enrollmentGetCmd)
 
-	initMultiOpArgFlags(enrollmentEmailCmd, "user", "send enrollment emails", "id", "[]int64")
+	initMultiOpArgFlags(enrollmentEmailCmd, "user", "send enrollment emails. If provided classification for this user does not exist, create one", "id", "[]int64")
+	enrollmentEmailCmd.Flags().Int("slots", 1, "specify the number of slots for the enrollment link")
+	enrollmentEmailCmd.Flags().String("classification", "supervised", "specify the classification to send the enrollment email for")
+	enrollmentEmailCmd.Flags().Bool("all", false, "send enrollment link to all classifications for the user")
 	initOutputFlags(enrollmentEmailCmd)
 	initLoopControlFlags(enrollmentEmailCmd)
 	initTenantFlags(enrollmentEmailCmd)
